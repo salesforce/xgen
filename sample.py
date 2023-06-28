81c170f4f732ac5c06adde74f0b7b0bf8e8efc96
Erik Nijkamp <erik.nijkamp@gmail.com>
2023-06-27 21:43:04 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,8 @@
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("checkpoints/xgen-7B")
-model = AutoModelForCausalLM.from_pretrained("checkpoints/xgen-7B", torch_dtype=torch.bfloat16, revision="sharded")
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-base", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("Salesforce/xgen-7b-8k-base", torch_dtype=torch.bfloat16)
 inputs = tokenizer("The world is", return_tensors="pt")
 sample = model.generate(**inputs, max_length=128)
 print(tokenizer.decode(sample[0]))
